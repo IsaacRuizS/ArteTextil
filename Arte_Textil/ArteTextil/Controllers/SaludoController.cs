fb884337263ae6bbf0ec644d661057694006f19d
IsaacGivi <isaacruizse@gmail.com>
2026-01-13 03:00:49 +0100

    Add back-end project

---
+++ Arte_Textil/ArteTextil/Controllers/SaludoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArteTextil.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SaludoController : ControllerBase
+    {
+        [HttpGet("hola")]
+        public IActionResult ObtenerSaludo()
+        {
+            return Ok("¡Hola desde el backend de ArteTextil!");
+        }
+    }
+}
---
+++ Arte_Textil/ArteTextil/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configuración del pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// NO usamos Auth todavía
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
